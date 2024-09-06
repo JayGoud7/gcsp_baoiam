37bfe591153e669d284cb60cc04c9fec31ef6eb0
Ajay goud <ajaygoud@Ajays-Air.bbrouter>
2024-09-06 20:19:21 +0530

    changes

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
+// import video from "../../assets/GSCP/favicon.mp4";
 
 const Section1 = () => {
   useEffect(() => {
@@ -45,24 +46,23 @@ const Section1 = () => {
             ease: "back.inOut",
             stagger: 0.3,
           },
-          // Start h2 animation before h1 finishes
+
           "-=0"
         )
-
         .to(sentence, { display: "none", duration: 2 });
     });
   }, []);
 
   return (
     <>
-      <section className="w-full h-full relative overflow-hidden xs:w-full xs:h-full">
+      <section className="w-full h-screen relative overflow-hidden px-4 xs:px-4">
         <video
           className="absolute top-0 left-0 w-full h-full object-cover"
           loop
           autoPlay
           muted
         >
-          <source src="./favicon.mp4" type="video/mp4" />
+          <source src='' type="video/mp4" />
           Your browser does not support HTML5 video.
         </video>
 
@@ -70,31 +70,31 @@ const Section1 = () => {
           <div className="w-full h-full bg-gradient-to-b from-transparent to-black opacity-60"></div>
         </div>
 
-        <div className="text-6xl text-white font-extrabold flex flex-col items-center justify-center w-full h-full relative xs:text-3xl xs:gap-2 xs:px-2">
-          <div className="sentence gap-4 flex items-center  absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]  xs:gap-2">
-            <h1 className="bg-red-500 p-2 mb-20 xs:mb-[14vw] xs:p-1 ">
-              <span>Responsive</span>
+        <div className="text-6xl text-white font-extrabold flex flex-col items-center justify-center w-full h-full relative xs:text-3xl">
+          <div className="sentence gap-4 flex items-center text-center absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] xs:gap-1">
+            <h1 className="bg-red-500 p-2 xs:p-1">
+              <span className="xs:text-[8vw]">Responsive</span>
             </h1>
             <span className="w-1 h-48 bg-black xs:h-32 spn"></span>
-            <h2 className="xs:text-2xl mt-20 xs:mt-[18vw]">Layout</h2>
-            <h2 className="xs:text-2xl mt-20 xs:mt-[18vw]">For</h2>
-            <h2 className="xs:text-2xl mt-20 xs:mt-[18vw]">Us.</h2>
+            <h2 className="xs:text-[7.5vw]">Layout</h2>
+            <h2 className="xs:text-[7.5vw]">For</h2>
+            <h2 className="xs:text-[7.5vw]">Us.</h2>
           </div>
-          <div className="sentence gap-4 flex items-center  absolute top-[50%] left-[49%] transform -translate-x-[50%] -translate-y-[50%]  xs:gap-2">
-            <h1 className="bg-red-500 p-2 mb-20 xs:mb-[14vw] xs:p-1 ">
-              <span>Responsive</span>
+          <div className="sentence gap-4 flex items-center text-center absolute top-[50%] left-[49%] transform -translate-x-[50%] -translate-y-[50%] xs:gap-1">
+            <h1 className="bg-red-500 p-2 xs:p-1">
+              <span className="xs:text-[8vw]">Responsive</span>
             </h1>
             <span className="w-1 h-48 bg-black xs:h-32 spn"></span>
-            <h2 className="xs:text-2xl mt-20 xs:mt-[18vw]">Better</h2>
-            <h2 className="xs:text-2xl mt-20 xs:mt-[18vw]">World.</h2>
+            <h2 className="xs:text-[7.5vw]">Better</h2>
+            <h2 className="xs:text-[7.5vw]">World.</h2>
           </div>
-          <div className="sentence gap-4 flex  items-center   absolute top-[50%] left-[42%] transform -translate-x-[50%] -translate-y-[50%]  xs:gap-2">
-            <h1 className="bg-red-500 p-2 mb-20 xs:mb-[14vw] xs:p-1">
-              <span>Responsive</span>
+          <div className="sentence gap-4 flex items-center text-center absolute top-[50%] left-[42%] transform -translate-x-[50%] -translate-y-[50%] xs:gap-1">
+            <h1 className="bg-red-500 p-2 xs:p-1">
+              <span className="xs:text-[8vw]">Responsive</span>
             </h1>
-            <span className="w-1 h-48 bg-black  xs:h-32 spn"></span>
-            <h2 className="xs:text-2xl mt-20 xs:mt-[18vw]">For</h2>
-            <h2 className="xs:text-2xl mt-20 xs:mt-[18vw]">Us.</h2>
+            <span className="w-1 h-48 bg-black xs:h-32 spn"></span>
+            <h2 className="xs:text-[7.5vw]">For</h2>
+            <h2 className="xs:text-[7.5vw]">Us.</h2>
           </div>
         </div>
       </section>
--- src/components/Section4.js
+++ src/components/Section4.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const Section4 = () => {
+const Section2 = ({ dark }) => {
   const [lettersRef, setlettersRef] = useArrayRef();
   const triggerRef = useRef(null);
 
@@ -24,8 +24,7 @@ const Section4 = () => {
         start: "top 60%",
         end: "bottom 85%",
       },
-      color: "#2A2A2A",
-
+      color: "black",
       stagger: 0.1,
     });
 
@@ -70,15 +69,15 @@ const Section4 = () => {
     return () => {
       anim.kill();
     };
-  });
+  }, [lettersRef, dark]);
 
   return (
-    <section className=" w-full h-full flex items-center justify-evenly flex-col">
-      <div className="w-11/12 h-1 bg-black border-expand"></div>
-      <div ref={triggerRef} className="px-12 xs:px-8">
+    <section className=" w-full h-screen flex items-center justify-evenly flex-col xs:py-0">
+      <span className="w-11/12 h-1 bg-black border-expand dark:bg-[#EB0027]"></span>
+      <div ref={triggerRef} className="px-12  xs:px-8">
         {text.split("").map((letter, index) => (
           <span
-            className="font-roboto leading-[5vw] font-medium text-[3.4vw] text-white drop-shadow-[0_0_0.03rem_rgb(2,2,2)] xs:leading-10  xs:font-medium xs:text-[7vw] xs:drop-shadow-[0_0_0.03rem_rgb(2,2,2)] xs:text-white"
+            className="font-roboto leading-[6vw] font-medium text-white text-[3.4vw] dark:drop-shadow-[0_0_0.03rem_#EB0027]   drop-shadow-[0_0_0.03rem_rgb(2,2,2)] xs:leading-[9vw]   xs:font-medium xs:text-[6vw]  xs:text-white"
             key={index}
             ref={setlettersRef}
           >
@@ -90,7 +89,7 @@ const Section4 = () => {
       <div className="what">
         <a
           href="#_"
-          className="relative inline-flex items-center px-12 py-2 overflow-hidden text-lg font-medium text-black border-2 border-black rounded-full hover:text-white group hover:bg-black"
+          className="relative inline-flex items-center px-12 py-2 overflow-hidden text-lg font-medium text-black dark:text-white dark:bg-[#EB0027] dark:border-[#EB0027] border-2 border-black rounded-full hover:text-white  dark:hover:text-[#EB0027] dark:hover:bg-[#EB0027] group"
         >
           <span className="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
           <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
@@ -113,9 +112,10 @@ const Section4 = () => {
         </a>
       </div>
 
-      <div className="w-11/12 h-1 bg-black border-expand"></div>
+      <span className="w-11/12 h-1 bg-black border-expand dark:bg-[#EB0027]"></span>
     </section>
   );
 };
 
-export default Section4;
+export default Section2;
+
